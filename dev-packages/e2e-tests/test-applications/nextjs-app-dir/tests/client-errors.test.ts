b58cf7097207ed72b9a5a5927432f250b7456f46
Luca Forstner <luca.forstner@sentry.io>
2024-07-08 10:36:30 +0000

    Undo version check

--- dev-packages/e2e-tests/test-applications/nextjs-app-dir/tests/client-errors.test.ts
+++ dev-packages/e2e-tests/test-applications/nextjs-app-dir/tests/client-errors.test.ts
@@ -1,12 +1,7 @@
 import { expect, test } from '@playwright/test';
 import { waitForError } from '@sentry-internal/test-utils';
 
-const packageJson = require('../package.json');
-
 test('Sends a client-side exception to Sentry', async ({ page }) => {
-  const nextjsVersion = packageJson.dependencies.next;
-  const nextjsMajor = Number(nextjsVersion.split('.')[0]);
-
   await page.goto('/');
 
   const errorEventPromise = waitForError('nextjs-app-dir', errorEvent => {
@@ -28,8 +23,6 @@ test('Sends a client-side exception to Sentry', async ({ page }) => {
   expect(errorEvent.transaction).toEqual('/');
 
   expect(errorEvent.contexts?.trace).toEqual({
-    // Next.js >= 15 propagates a trace ID to the client via a meta tag.
-    parent_span_id: nextjsMajor >= 15 ? expect.any(String) : undefined,
     trace_id: expect.any(String),
     span_id: expect.any(String),
   });
